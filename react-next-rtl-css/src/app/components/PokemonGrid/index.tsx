c38b282914a9f7c99445641397968de26bbba598
Matt Aizlewood <aizlewood40@live.co.uk>
2025-09-27 20:40:55 +0100

    feat(react-next-rtl-css): query more data when scrolling

--- react-next-rtl-css/src/app/components/PokemonGrid/index.tsx
+++ react-next-rtl-css/src/app/components/PokemonGrid/index.tsx
@@ -6,16 +6,33 @@ import styles from './index.module.css';
 const PokemonGrid: React.FC = () => {
 
     const [pokemon, setPokemon] = React.useState<PokemonResult[]>([]);
+    const [offset, setOffset] = React.useState(0);
 
     React.useEffect(() => {
         // Fetch and display Pokémon data here
         const fetchData = async () => {
-            const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=50');
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=${offset}`);
             const data = await response.json();
-            setPokemon(data?.results);
+            setPokemon((d) => [...d, ...data?.results]);
         };
         fetchData();
-    }, []);
+    }, [offset]);
+
+    React.useEffect(() => {
+        const handleScroll = () => {
+                const { scrollTop, scrollHeight, clientHeight } = window.document.documentElement;
+                // Check if user has scrolled to within 200px of the bottom
+                if (scrollTop + clientHeight >= scrollHeight - 800) {
+                    setOffset(offset + 25);
+                }
+        };
+
+            window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [pokemon]);
 
     return (
         <div className={styles.grid}>
